104e8a54d9c3625423a80b4bece7752a80afc277
Trevor Jameson <tjameson15@gmail.com>
2018-06-24 12:30:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,12 +19,13 @@ let fidelity = 0;
 document.body.addEventListener('keydown', function konamiCode(press) {
     // Assign inputted value to var
     userInput = (press.key);
-    
+
     // Check user input for fidelity against Konami code
         if (codes[fidelity] === userInput) {
             fidelity++;
         } else if (codes[fidelity] !== userInput) {
             fidelity = 0;
+            userInput = "";
         }
 
     // Check fidelity count of array comparison and alert the user if successful
